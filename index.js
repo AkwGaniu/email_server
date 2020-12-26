abe06bf233e46c56c7e7d1ca241324ed7aefcbca
AkwGaniu <lmd4sure@gmail.com>
2020-12-26 17:53:49 +0100

    some fix

--- index.js
+++ index.js
@@ -32,49 +32,55 @@ app.use((req, resp, next) => {
     } else {
         next()
     }
-
 })
 
 app.post("/", async (req, resp, next) => {
-  
-  const htmlToSend = template({
-    meeting_title: 'data.title',
-    meeting_id: 'data.meeting_id',
-    host: 'host_name.names',
-    meeting_date: 'data.date',
-    start_time: 'data.from',
-    end_time: 'data.to'
-  })
-
-  const mailOptions = {
-    from: process.env.USER,
-    to: "lmd4sure@gmail.com",
-    subject: "Virtual Meeting Details",
-    html: htmlToSend
-  }
-
-  smtpTransport.sendMail(mailOptions, function(error, response) {
-    if (error) {
-      next(error)
-      console.log(error)
+  try {
+    console.log(req.body)
+    const reciever = req.body.to
+    const subject = req.body.subject
+    if (reciever === '' || subject === '') {
+      throw new Error('Invalid parameters')
     } else {
-      console.log(response)
-      resp.json({
-        error: 0,
-        message: 'Success'
+      const htmlToSend = template({
+        meeting_title: 'data.title',
+        meeting_id: 'data.meeting_id',
+        host: 'host_name.names',
+        meeting_date: 'data.date',
+        start_time: 'data.from',
+        end_time: 'data.to'
+      })
+      const mailOptions = {
+        from: process.env.USER,
+        to: reciever,
+        subject: subject,
+        html: htmlToSend
+      }
+      smtpTransport.sendMail(mailOptions, function(error, response) {
+        if (error) {
+          throw error
+        } else {
+          resp.json({
+            error: 0,
+            message: 'Success'
+          })
+        }
       })
     }
-  })
+  } catch (error) {
+    next(error)
+  }
+
 });
 
 //Custom Error Handler middleware
 app.use((error, req, resp, next) => {
-    resp.status(error.status || 500)
-    resp.json({
-        status: error.status,
-        message: error.message,
-        stack: error.stack
-    })
+  resp.status(error.status || 500)
+  resp.json({
+    status: error.status,
+    message: error.message,
+    // stack: error.stack
+  })
 })
 
 const PORT = process.env.PORT || 3000
